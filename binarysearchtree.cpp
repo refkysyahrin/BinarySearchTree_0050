21ac9d14990eed81e3c27993e684512027889984
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-13 21:28:05 +0700

    This function searches the currentNode of the specified Node as well as the current Node of its parent

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -1,11 +1,11 @@
-#include <iostring>
+#include <iostream>
 #include <string>
 using namespace std;
 
 class Node
 {
 public:
-    string info:
+    string info;
     Node* leftchild;
     Node* rightchild;
 
@@ -55,111 +55,11 @@ public:
         }
     }
 
-    void search(string element, Node*& parent, Node*& currentNode)
-	{
-		currentNode = ROOT;
-		parent = NULL;
-		while ((currentNode != NULL) && currentNode->info != element)
-		{
-			parent = currentNode;
-			if (element < currentNode->info)
-				currentNode = currentNode->leftchild;
-			else
-				currentNode = currentNode->rightchild; 
-		}
-	}
-	void inorder(Node* ptr)
-	{
-		if (ROOT == NULL)
-		{
-			cout << "Tree is empety" << endl;
-			return;
-		}
-		if (ptr != NULL)
-		{
-			inorder(ptr->leftchild);
-			cout << ptr->info << " ";
-			inorder(ptr->rightchild);
-		}
-	}
-	void preorder(Node* ptr)
-	{
-		if (ROOT == NULL)
-		{
-			cout << "Tree is empety" << endl;
-			return;
-		}
-		if (ptr != NULL)
-		{
-			cout << ptr->info << " ";
-			preorder(ptr->leftchild);
-			preorder(ptr->rightchild);
-		}
-	}
-	void postorder(Node* ptr)
-	{
-		if (ROOT == NULL)
-		{
-			cout << "Tree is empety" << endl;
-			return;
-		}
-		if (ptr != NULL)
-		{
-			postorder(ptr->leftchild);
-			postorder(ptr->rightchild);
-			cout << ptr->info << " ";
-		}
-	}
-};
 
-int main() {
-	BinaryTree x;
-	while (true) {
-		cout << "\nMenu" << endl;
-		cout << "1. Implement insert operation" << endl;
-		cout << "2. Perform inorder traversal" << endl;
-		cout << "3. Perform preorder traversal" << endl;
-		cout << "4. Perform postorder traversal" << endl;
-		cout << "5. Exit" << endl;
-		cout << "\nEnter your choice (1-5) : ";
-
-		char ch;
-		cin >> ch;
-		cout << endl;
-		
-		switch (ch)
-		{
-			case '1': 
-			{
-				cout << "Enter a word: ";
-				string word;
-				cin >> word;
-				x.insert(word);
-				break;
-			}
-			case '2':
-			{
-				x.inorder(x.ROOT);
-				break;
-			}
-			case '3':
-			{
-				x.preorder(x.ROOT);
-				break;
-			} 
-			case '4': {
-				x.postorder(x.ROOT);
-				break;
-			}
-			case '5': {
-				return 0;
-			}
-
-			default:
-				cout << "Pilihan tidak ditemukan" << endl;
-				break;
-			
-		}
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// This function searches the currentNode of the specified Node as well as the current Node of its parent
 		
 	}
-};
+
+    
