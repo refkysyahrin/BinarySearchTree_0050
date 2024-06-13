8d53167c45741b6a061c908cc58bd44f5fb342df
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-13 18:30:51 +0700

    Make the right child of the new node point to NULL

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -33,5 +33,6 @@ public:
         Node* newNode = new Node(element, nullptr, nullptr); // Allocated memory for the new node
         newNode->info = element; // Assign value to the data field of the new data
         newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; // Make the right child of the new node point to NULL
     }
 }
