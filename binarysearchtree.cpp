c0e34118d821622b52237df05ac9c57346fca56d
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-13 19:15:16 +0700

    selesai

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -54,4 +54,112 @@ public:
             parent->rightchild = newNode // Make te right child of the parent point to the  new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+	{
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && currentNode->info != element)
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild; 
+		}
+	}
+	void inorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empety" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empety" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+	void postorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empety" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+};
+
+int main() {
+	BinaryTree x;
+	while (true) {
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+		
+		switch (ch)
+		{
+			case '1': 
+			{
+				cout << "Enter a word: ";
+				string word;
+				cin >> word;
+				x.insert(word);
+				break;
+			}
+			case '2':
+			{
+				x.inorder(x.ROOT);
+				break;
+			}
+			case '3':
+			{
+				x.preorder(x.ROOT);
+				break;
+			} 
+			case '4': {
+				x.postorder(x.ROOT);
+				break;
+			}
+			case '5': {
+				return 0;
+			}
+
+			default:
+				cout << "Pilihan tidak ditemukan" << endl;
+				break;
+			
+		}
+		
+	}
 };
