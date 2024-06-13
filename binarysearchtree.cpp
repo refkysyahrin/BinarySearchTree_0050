ed719aa3e07686a6ea556adb43dae9cf8fe610cd
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-13 21:56:08 +0700

    invalid option

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -59,7 +59,139 @@ public:
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
 		// This function searches the currentNode of the specified Node as well as the current Node of its parent
-		
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
 	}
+
+	void inorder(Node* ptr)
+	{
+		id (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+
+	void preorder(Node* ptr)
+	{
+		if (ROOT == null)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
+
+	void postorder (Node* ptr)
+	{
+		// Perfoms the postorder traversal of the tree
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
+};
+
+int main()
+{
+	BinaryTree obj;
+	while (true)
+	{
+		cout << "\nMenu" << endl;
+		cout << "1. Implement insert operation" << endl;
+		cout << "2. Perform inorder traversal" << endl;
+		cout << "3. Perform preorder traversal" << endl;
+		cout << "4. Perform postorder traversal" << endl;
+		cout << "5. Exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		car ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word : ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case '2';
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
+		case '3';
+		{
+			obj.preorder(obj.ROOT);
+			break;
+		}
+		case '4';
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case '5';
+			return 0;
+		defalt:
+		} 
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+		}
+	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
